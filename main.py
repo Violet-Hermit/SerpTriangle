4fc00beebb443a615d6e342fa27673ce31d0b666
Violet-Hermit <100461599+Violet-Hermit@users.noreply.github.com>
2022-02-26 16:59:52 +0300

    Add files via upload
    upgrade queue
    add pruning

--- main.py
+++ main.py
@@ -6,13 +6,21 @@
 from random import *
 from tkinter import *
 import collections
-
+import math
 
 
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+#Check whether a pointwith a simular rounded value was present earlier in the queue
+def find_dif(l,elm):
+    for i in range(len(l)):
+        if int(l[i].x)-int(elm.x)==0 and int(l[i].y)-int(elm.y)==0:
+            return False
+    l.append(elm)
+    return True
 
 
 def prop_div(p1, p2): # return middle of segment
@@ -22,15 +30,18 @@ def prop_div(p1, p2): # return middle of segment
 def draw_point(p): # draw point
     canvas.create_line(p.x, p.y, p.x + 1, p.y, fill=colors)
 
+
 #Get pointers from the queue
 #Function draw all available point for this pointer
+#Check whether a pointwith a simular rounded value was present earlier in the queue
 #Add result to queue
-def new_point(nodes, dq):
+def new_point(nodes, dq, l):
     point = dq.popleft()
     for i in range(len(nodes)):
         pnt = prop_div(nodes[i],point)
-        draw_point(pnt) # draw point
-        dq.append(pnt) # add new elements from queue
+        if find_dif(l, pnt):
+            draw_point(pnt) # draw point
+            dq.append(pnt) # add new elements from queue
 
 
 size = 1000 # size of screen
@@ -42,6 +53,8 @@ nodes = [Point(10, 10) , Point(910, 10), Point(460, 910)] # nodes of triangle
 pt = Point(10,10) # pointer
 dq = collections.deque()
 dq.append(pt)
+l = list()
 while True:
-    new_point(nodes, dq)
+    print(len(dq))
+    new_point(nodes, dq, l)
     wnd.update() # update
